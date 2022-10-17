d2b21c449ad04b18d4a2905611f2aad354a1bfc5
chcodes <chandrahassan15@gmail.com>
2022-10-17 10:30:56 +0530

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+# JavaScript Project - A Simple Todo App
+
+## **Project: TODO APP**
+
+![Badge](https://img.shields.io/badge/Javascript-TODO-brightgreen " Simple TODO App")
+
+![Badge with LCO and ineuron tagged](https://img.shields.io/badge/Ineuron.ai-LCO-brightgreen)
+
+![Badge with LCO and ineuron tagged](https://img.shields.io/badge/Full%20Stack%20JavaScript%20bootcamp-Hitesh%20Choudhary-brightgreen)
+
+### Concepts learned from this project:
+- Created a simple Todo App using DOM manipulation without localstorage.
+- This is a practice project mainly to play with DOM Elements.
+
+
+**Live Link :** [Deployed Link](https://js-project-todoapp.netlify.app/)
+>### Preview :
+
+![Homepage screenshot-1](preview.png)
---
+++ index.js
@@ -0,0 +1,59 @@
+const input = document.querySelector(".input");
+const output = document.querySelector(".addButton");
+const container = document.querySelector(".container");
+// id for each todo
+let i = 0;
+//To do Add button
+output.addEventListener("click", (e) => {
+  let newElem = document.createElement("div");
+  let editElem = document.createElement("span");
+  let delElem = document.createElement("span");
+  e.preventDefault();
+  // edit button
+  editElem.innerHTML = "EDIT";
+  editElem.classList.add("editButton");
+  // delete button
+  delElem.innerHTML = "DELETE";
+  delElem.classList.add("deleteButton");
+  // new todo
+  newElem.innerHTML = `${input.value}`;
+  newElem.setAttribute("id", i++);
+  // cssText way of adding styles to an element
+  newElem.style.cssText += `font-size: 3rem;
+  font-weight: 50;
+  margin: 1rem 0 3rem;
+  text-align: center;
+  color: rgb(255, 255, 255);
+  font-family: "Bebas Neue", cursive;`;
+  newElem.classList.add("item");
+
+  newElem.append(editElem);
+  newElem.append(delElem);
+  container.insertAdjacentElement("afterbegin", newElem);
+  container.classList.add("container");
+
+  // edit btn on click
+  editElem.addEventListener("click", (e) => {
+    e.preventDefault();
+    let editableId = editElem.parentElement.id;
+    let editable = editElem.previousSibling.textContent;
+    let newVal = prompt("enter what you need to update", editable);
+    if (newVal !== "") {
+      let newOne = document.getElementById(`${editableId}`);
+      newOne.textContent = newVal;
+      newOne.append(editElem);
+      newOne.append(delElem);
+    } else {
+      alert("You didn't updated any valid one");
+    }
+  });
+  // delete button onclick
+  delElem.addEventListener("click", () => {
+    setTimeout(() => {
+      delElem.parentElement.remove();
+    }, 500);
+  });
+  // clear input field
+  input.value = "";
+});
+//
